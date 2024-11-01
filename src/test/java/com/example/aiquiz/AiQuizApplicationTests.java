ce0dea38bffdcc506629db870d4fcec926fd9bfe
gyuchangShim <shim17@mju.ac.kr>
2024-11-01 10:27:38 +0900

    build: project setting

---
+++ src/test/java/com/example/aiquiz/AiQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
