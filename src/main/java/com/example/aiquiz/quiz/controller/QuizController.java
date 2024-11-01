cf1a61222707fba0e3a28d2f2f1dff23485028a8
gyuchangShim <shim17@mju.ac.kr>
2024-11-01 11:04:06 +0900

    style: quizcontroller tag 추가

--- src/main/java/com/example/aiquiz/quiz/controller/QuizController.java
+++ src/main/java/com/example/aiquiz/quiz/controller/QuizController.java
@@ -1,4 +1,7 @@
 package com.example.aiquiz.quiz.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Quiz API", description = "문제 관련 API")
 public interface QuizController {
 }
