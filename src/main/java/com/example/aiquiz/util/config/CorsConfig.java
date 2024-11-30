4db710a84d409f09a49cbdc90c890da6ac1deb8a
gyuchangShim <shim17@mju.ac.kr>
2024-11-30 14:36:35 +0900

    fix: cors origin setting 수정

--- src/main/java/com/example/aiquiz/util/config/CorsConfig.java
+++ src/main/java/com/example/aiquiz/util/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://43.201.19.254:5173"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5173/*",
+			"http://43.201.19.254:5173", "http://43.201.19.254:5173/*"));
 		// 프론트엔드 주소 List.of("http://localhost:5173","ec2-52-79-52-146.ap-northeast-2.compute.amazonaws.com")
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
 		configuration.setAllowedHeaders(List.of("*"));
